d8a8bb8e2b9a0f3694fdc1879c98e928ccfc13f8
mi-gand <m.gandrabura@mail.ru>
2021-05-17 16:56:52 +0300

    edits XO

--- 11_6_5_XO.cpp
+++ 11_6_5_XO.cpp
@@ -3,7 +3,7 @@
 
 const int width = 3;
 const int height = 3;
-const int8_t Incorrect = 0;														//для исключений
+const int8_t Incorrect = 0;														//для исключений для управлением потоком (что неправильно, но тестирую exception)
 const int16_t Nobody = 0;
 const int32_t PetyaWin = 0;
 const int64_t VasyaWin = 0;
@@ -14,7 +14,7 @@ void finalCheck(const char gameBoard[height][width], bool personWin1, bool perso
 int main()
 {
 	setlocale(LC_ALL, "ru");
-	char gameBoard[height][width]{ 0 };
+	char gameBoard[height][width] = { {} };
 	bool flagValidSymbol{ 0 };
 
 	do
@@ -25,7 +25,7 @@ int main()
 		std::cin >> gameBoard[1];
 		std::cin >> gameBoard[2];
 		validSymbol(gameBoard, flagValidSymbol);
-	}while (!flagValidSymbol);
+	} while (!flagValidSymbol);
 
 	try
 	{
@@ -52,6 +52,8 @@ int main()
 	{
 		std::cout << "VasyaWin";
 	}
+
+	return 0;
 }
 
 
@@ -78,29 +80,28 @@ void validSymbol(char gameBoard[height][width], bool& flagOut)
 }
 
 bool checkWin(const char gameBoard[height][width], char symbol)
-{	
+{
 
-		int win = 0;
-		if (gameBoard[0][0] == symbol and gameBoard[0][1] == symbol and gameBoard[0][2]) win++;
-		if (gameBoard[1][0] == symbol and gameBoard[1][1] == symbol and gameBoard[1][2]) win++;
-		if (gameBoard[2][0] == symbol and gameBoard[2][1] == symbol and gameBoard[2][2]) win++;
+	int win = 0;
+	if (gameBoard[0][0] == symbol and gameBoard[0][1] == symbol and gameBoard[0][2] == symbol) win++;
+	if (gameBoard[1][0] == symbol and gameBoard[1][1] == symbol and gameBoard[1][2] == symbol) win++;
+	if (gameBoard[2][0] == symbol and gameBoard[2][1] == symbol and gameBoard[2][2] == symbol) win++;
 
-		if (gameBoard[0][0] == symbol and gameBoard[1][0] == symbol and gameBoard[2][0]) win++;
-		if (gameBoard[0][1] == symbol and gameBoard[1][1] == symbol and gameBoard[2][1]) win++;
-		if (gameBoard[0][2] == symbol and gameBoard[1][2] == symbol and gameBoard[2][2]) win++;
+	if (gameBoard[0][0] == symbol and gameBoard[1][0] == symbol and gameBoard[2][0] == symbol) win++;
+	if (gameBoard[0][1] == symbol and gameBoard[1][1] == symbol and gameBoard[2][1] == symbol) win++;
+	if (gameBoard[0][2] == symbol and gameBoard[1][2] == symbol and gameBoard[2][2] == symbol) win++;
 
-		if (gameBoard[0][0] == symbol and gameBoard[1][1] == symbol and gameBoard[2][2]) win++;
-		if (gameBoard[0][2] == symbol and gameBoard[1][1] == symbol and gameBoard[2][0]) win++;
-		std::cout << "Строка - "<<gameBoard[0][2] << gameBoard[1][1] << gameBoard[2][0] << std::endl;
+	if (gameBoard[0][0] == symbol and gameBoard[1][1] == symbol and gameBoard[2][2] == symbol) win++;
+	if (gameBoard[0][2] == symbol and gameBoard[1][1] == symbol and gameBoard[2][0] == symbol) win++;
 
-		if (win > 1) throw Incorrect;
-		if (win == 0) return false;
-		if (win == 1) return true;
-	
+	if (win > 1) throw Incorrect;
+	if (win == 0) return false;
+	if (win == 1) return true;
+
 }
 
 void finalCheck(const char gameBoard[height][width], bool personWin1, bool personWin2, char Person1, char Person2)
-{	
+{
 	if (personWin1 == 1 and personWin2 == 1) throw Incorrect;
 	int countX_Petya1{ 0 };
 	int countY_Vasya2{ 0 };
@@ -111,8 +112,8 @@ void finalCheck(const char gameBoard[height][width], bool personWin1, bool perso
 			if (gameBoard[i][j] == Person1)	countX_Petya1++;
 			if (gameBoard[i][j] == Person2)	countY_Vasya2++;
 		}
-		if (personWin1 and countY_Vasya2 > countX_Petya1) throw Incorrect;
-		if (personWin2 and countX_Petya1 > countY_Vasya2) throw Incorrect;
+		if (personWin1 and countY_Vasya2 >= countX_Petya1) throw Incorrect;
+		if (personWin2 and countX_Petya1 >= countY_Vasya2) throw Incorrect;
 		if (personWin1 == 0 and personWin2 == 0 and countX_Petya1 <= 5 and countY_Vasya2 <= 4) throw Nobody;
 		if (personWin1 == 1) throw PetyaWin;
 		if (personWin2 == 1) throw VasyaWin;
