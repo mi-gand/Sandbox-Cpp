adadc31be37a96f8c18902f3cb8869a3aa1b7f18
mi-gand <m.gandrabura@mail.ru>
2021-05-20 15:56:19 +0300

    ring_buffer_lite_without_Class

---
+++ 13_6_1_ring_buffer_lite.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+	std::vector <int> _array(20);
+	int curpos = 0;
+	int input = -3;
+	std::cout << "Enter numbers of array: \n";
+	while (input != -2)
+	{
+		std::cin >> input;
+		if (input == -1)
+		{
+			for (int i = curpos; i < _array.size(); i++)
+			{
+				std::cout << _array[i] << " ";
+			}
+			for (int i = 0; i < curpos; i++)
+			{
+				std::cout << _array[i] << " ";
+			}
+			std::cout << std::endl;
+		}
+		else
+		{
+			_array[curpos] = input;
+			curpos++;
+			if (curpos == 20) curpos = 0;
+		}
+	}
+	std::cout << "END";
+	return 0;
+}
