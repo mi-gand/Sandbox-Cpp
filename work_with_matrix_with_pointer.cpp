3d3d77c175877602a3900eb6c3a978238cf6c6cf
mi-gand <m.gandrabura@mail.ru>
2021-06-03 15:50:55 +0300

    pointer**

---
+++ work_with_matrix_with_pointer.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+void foo(bool** arr);
+
+int main()
+{
+	bool** arr = new bool* [12];
+	foo(arr);
+
+	for (int i = 0; i < 12; ++i)
+	{
+		for (int j = 0; j < 12; ++j)
+		{
+			std::cout << arr[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+void foo(bool** arr)
+{
+	for(int i = 0; i < 12; ++i)
+	{
+		arr[i] = new bool[12];
+		for (int j = 0; j < 12; ++j)
+		{
+			arr[i][j] = 0;
+		}
+	}
+}
